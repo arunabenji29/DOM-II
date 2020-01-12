44248190b273f4328925569dbbc8d3d94acb3057
Aruna Benjamin <arunabenji29@gmail.com>
2020-01-11 18:54:54 -0700

    played with the drag and drop

--- js/index.js
+++ js/index.js
@@ -59,22 +59,162 @@ navBar.addEventListener('click',function(event){
     console.log('cant load')
 })
 
-//drag items
-let image2 = document.querySelector('.rounded')
-image2.addEventListener('dragend',function(event){
-    event.preventDefault();
-    event.target.style.transform = 'scale(2)'
-    console.log(`dragend: ${event.target}`)
-})
+// //drag ad drop
+// const fill = document.querySelector('.fill')
+// const image22 = document.querySelector('.img-content1')
+
+// fill.addEventListener('dragstart',dragstart);
+// image22.addEventListener('dragleave',dragleave)
+// image22.addEventListener('dragenter',dragenter)
+// image22.addEventListener('dragover',dragover)
+// fill.addEventListener('dragend',dragend)
+// image22.addEventListener('drop',drop)
+
+// function dragstart(){
+//     // console.log('start')
+//     this.className += ' hold';
+//     setTimeout(() => {
+//         this.className = 'invisible'
+//         fill.style.display='none'
+//     },1000)
+// }
+
+// function dragend(){
+//     // console.log('end')
+//     this.className = 'fill'
+// }
+
+// function dragleave(){
+//     // console.log('dragleave')
+//     this.className = 'img-content1'
+// }
+
+// function dragenter(event){
+//     event.preventDefault()
+//     this.className += ' hovered'
+//     console.log('dragenter')
+// }
+
+// function dragover(event){
+//     event.preventDefault()
+//     console.log('dragover')
+// }
+
+// function drop(){
+//     console.log('drop')
+//     this.className = 'img-content1'
+//     this.append()
+// }
+
+
+//last section drag and drop
+const lastSection1 = document.querySelector('.last-section1')
+const lastSection2 = document.querySelector('.last-section2')
+
+lastSection1.addEventListener('dragstart',dragstart);
+lastSection2.addEventListener('dragleave',dragleave)
+lastSection2.addEventListener('dragenter',dragenter)
+lastSection2.addEventListener('dragover',dragover)
+lastSection1.addEventListener('dragend',dragend)
+lastSection2.addEventListener('drop',drop)
+
+function dragstart(){
+    // console.log('start')
+    this.className += ' hold';
+    setTimeout(() => {
+        this.className = 'invisible'
+        // fill.style.display='none'
+    },1000)
+}
+
+function dragend(){
+    // console.log('end')
+    this.className = 'last-section1'
+}
+
+function dragleave(){
+    // console.log('dragleave')
+    this.className = 'empty'
+}
+
+
+function dragenter(event){
+    event.preventDefault()
+    this.className += ' hovered'
+    // console.log('dragenter')
+}
+
+function dragover(event){
+    event.preventDefault()
+    console.log('dragover')
+}
+
+function drop(){
+    console.log('drop')
+    this.className = 'empty'
+    this.append(lastSection1)
+}
+
+// //mousedown
+// let image1 = document.querySelector('.round')
+// image1.onmousedown = function(event){
+//     image1.style.position = 'absolute'
+//     image1.style.zIndex = 1000;
+//     document.body.append(image1)
+
+//     moveAt(event.pageX, event.pageY)
+
+//     function moveAt(pageX,pageY){
+//         image1.style.left = pageX - image1.offsetWidth/2 + 'px'
+//         image1.style.top = pageY - image1.offsetHeight/2 + 'px'
+//     }
+
+//     function onMouseMove(event){
+//         moveAt(event.pageX, event.pageY)
+//     }
+
+//     document.addEventListener('mousemove', onMouseMove)
+
+//     image1.onmouseup = function(){
+//         document.removeEventListener('mousemove',onMouseMove)
+//         image1.onmouseup = null;
+//     }
+
+//     image1.ondragstart = function(){
+//         return false
+//     }
+// }
+
+// //drag items
+// let image1 = document.querySelector('.round')
+
+// image1.addEventListener('dragstart',function(event){
+//     var code = '<img src="img/adventure.jpg">'
+//     event.dataTransfer.setData("Text", code);
+// //     event.preventDefault();
+// //     event.target.style.transform = 'scale(2)'
+//     console.log(`drag: ${event.target}`)
+// })
+
+// function drag(ev) {
+//     ev.dataTransfer.setData("text", ev.target.id);
+//     }
 
 
 //drop items
-let image1 = document.querySelector('.round')
-document.addEventListener('drop',function(event){
+let image2 = document.querySelector('.rounded')
+image2.addEventListener('drop',function(event){
     event.preventDefault();
+    image2.innerHTML = event.dataTransfer.getData("Text");
+    // image2.setAttribute('src', data)
+    // event.target.appendChild(data); 
+//     event.preventDefault();
     console.log(`drop: ${event.target}`)
 })
 
+// ev.preventDefault();
+//   var data = ev.dataTransfer.getData("text");
+//   ev.target.appendChild(document.getElementById(data));
 
 
 //scroll
